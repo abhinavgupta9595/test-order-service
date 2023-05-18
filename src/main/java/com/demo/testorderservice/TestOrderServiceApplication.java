0e692406477324c67d801d38618270febe6f124a
abgpt9595 <abgpt9595@outlook.com>
2023-05-18 12:05:07 +0530

    First Commit

---
+++ src/main/java/com/demo/testorderservice/TestOrderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.demo.testorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOrderServiceApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/demo/testorderservice/TestOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.testorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
